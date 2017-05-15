cf035b6e8a0200e00132adb85f0e033b14543d93
Jeffrey Becker <jabecker@jmt.com>
2017-05-15 11:28:57 -0400

    tests

---
+++ XUnitTestProject1/Db/SqlServerGeometryLoader.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coordinate.Domain.Db
+{
+    public class SqlServerGeometryLoader
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr LoadLibrary(string libname);
+
+        /// <summary>
+        /// Loads the required native assemblies for the current architecture (x86 or x64)
+        /// </summary>
+        /// <param name="rootApplicationPath">
+        /// Root path of the current application. Use Server.MapPath(".") for ASP.NET applications
+        /// and AppDomain.CurrentDomain.BaseDirectory for desktop applications.
+        /// </param>
+        public static void LoadNativeAssemblies(string rootApplicationPath)
+        {
+            var nativeBinaryPath = IntPtr.Size > 4
+                ? Path.Combine(rootApplicationPath, @"Db\nativeBinaries\x64\")
+                : Path.Combine(rootApplicationPath, @"Db\nativeBinaries\x86\");
+
+            LoadNativeAssembly(nativeBinaryPath, "msvcr120.dll");
+            LoadNativeAssembly(nativeBinaryPath, "SqlServerSpatial110.dll");
+        }
+
+        private static void LoadNativeAssembly(string nativeBinaryPath, string assemblyName)
+        {
+            var path = Path.Combine(nativeBinaryPath, assemblyName);
+            var ptr = LoadLibrary(path);
+            if (ptr == IntPtr.Zero)
+            {
+                throw new Exception($"Error loading {assemblyName} (ErrorCode: {Marshal.GetLastWin32Error()})");
+            }
+        }
+    }
+}
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Linq;
+using Coordinate.Domain.Db;
+using FluentMigrator.NHibernate;
+using GeoAPI.Geometries;
+using Xunit;
+using NHibernate;
+using NHibernate.Dialect;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+using NHibernate.Spatial.Dialect;
+
+namespace XUnitTestProject1
+{
+    public class CanExportGeometryTypes
+    {
+        [Fact]
+        public void Do()
+        {
+            SqlServerGeometryLoader.LoadNativeAssemblies(@"C:\code\FluentMigrator.NHibernate\XUnitTestProject1");
+            var configuration = new ExampleConfiguration();
+            configuration.BuildMappings();
+            Dialect dialect = Dialect.GetDialect(configuration.Properties);
+           var exprs= new ExpressionExporter(configuration, dialect).ToList();
+        }
+
+        private class ExampleConfiguration : NHibernate.Cfg.Configuration
+        {
+            public ExampleConfiguration()
+            {
+                Properties["dialect"] = typeof(MsSql2012GeometryDialect).AssemblyQualifiedName;
+                Properties["format_sql"] = "true";
+                //This will get overwritten by the configuration system
+                Properties["connection.connection_string"] =
+                    "Data Source=(local);Database=CoordinatePA;Integrated Security=True;Application Name=CoordinatePA;Type System Version=SQL Server 2012";
+                var mm = new ModelMapper();
+                mm.AddMapping(typeof(ExampleMapping));
+                AddDeserializedMapping(mm.CompileMappingForAllExplicitlyAddedEntities(),"example.hbm.xml");
+
+
+            }
+        }
+
+        public class ExampleGeoEntity
+        {
+            public virtual int Id { get; set; }
+            public virtual IGeometry Shape { get; set; }
+        }
+
+        public class ExampleMapping : ClassMapping<ExampleGeoEntity>
+        {
+            public ExampleMapping()
+            {
+                Id(x=>x.Id, m=>{m.Generator(Generators.Native);});
+                Property(x=>x.Shape, p =>
+                {
+                    p.NotNullable(true);
+                    p.Type(typeof(NHibernate.Spatial.Type.MsSql2008GeometryType), new { srid = 4326, subtype="GEOMETRY" });
+                    p.Column(c =>
+                    {
+                        c.SqlType("geometry");
+                    });
+                });
+            }
+        }
+    }
+}
